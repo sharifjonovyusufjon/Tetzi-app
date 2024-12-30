084042d2d6b544fd8c2e78883cb199cbb8c94715
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2024-12-31 02:13:33 +0900

    fix: memberController

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -59,7 +59,7 @@ memberController.login = async (req: Request, res: Response) => {
 memberController.logout = (req: ExtendedRequest, res: Response) => {
   try {
     console.log("logout");
-    res.cookie("access", null, { maxAge: 0, httpOnly: true });
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
     res.json({ logout: true });
   } catch (err) {
     console.log("Error, logout:", err);
